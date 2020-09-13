68d2b2b4477c5c929c1a88c6ebfae11c2e6502a1
CGMossa <cgmossa@gmail.com>
2020-09-13 08:52:11 +0200

    Removed unnecessary use of libc::int32_t. (#7)

--- src/myrustlib/src/random.rs
+++ src/myrustlib/src/random.rs
@@ -1,8 +1,7 @@
-use libc::int32_t;
 use rand::random;
 
 #[no_mangle]
-pub extern fn random_number() -> int32_t {
+pub extern fn random_number() -> i32 {
   let tuple = random::<i32>();
-  tuple as int32_t
+  tuple
 }
