a64cf4eac0103c6d63a47e9ed250a16b37d0170b
Jeroen Ooms <jeroenooms@gmail.com>
2022-11-18 15:46:10 +0100

    Update manual a bit

--- R/hello.R
+++ R/hello.R
@@ -1,6 +1,11 @@
 #' Hello Rust!
 #'
-#' Examples of rust functions via C.
+#' Minimal examples of calling rust functions in R via C.
+#'
+#' These functions call out to rust functions defined in the `myrustlib` cargo
+#' crate which is embedded in this package. They return values generated in Rust,
+#' such as a UTF-8 string or random number. In addition, `runthreads` is an
+#' example of a multi-threaded rust function.
 #'
 #' @export
 #' @rdname hellorust
