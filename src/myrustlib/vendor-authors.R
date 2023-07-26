e294a7839301ff7302add0c18b8522643362b82f
Jeroen Ooms <jeroenooms@gmail.com>
2023-07-26 17:15:03 +0200

    Strip email addresses from authors file

--- src/myrustlib/vendor-authors.R
+++ src/myrustlib/vendor-authors.R
@@ -2,7 +2,7 @@ if(!require('jsonlite')) install.packages('jsonlite', repos = 'https://cloud.r-p
 metadata <- jsonlite::fromJSON(pipe("cargo metadata --format-version 1"))
 packages <- metadata$packages
 stopifnot(is.data.frame(packages))
-authors <- vapply(packages$authors, function(x) paste(x, collapse = ', '), character(1))
+authors <- vapply(sub("<.*>", "", packages$authors), function(x) paste(x, collapse = ', '), character(1))
 lines <- sprintf(" - %s %s: %s", packages$name, packages$version, authors)
 dir.create('../../inst', showWarnings = FALSE)
-writeLines(c('Vendored cargo crates', lines), '../../inst/AUTHORS')
+writeLines(c('Authors of vendored cargo crates', lines), '../../inst/AUTHORS')
